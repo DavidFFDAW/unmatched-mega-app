510a08d36791d44510b0bd3a863a042236cdaea8
DavidFFDAW <davidferflo2@gmail.com>
2023-08-16 18:07:24 +0200

    style: unlimited decks dark background

---
+++ src/routes/unlimited-decks/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { onDestroy, onMount } from 'svelte';
+	const dark = 'dark-background';
+
+	onMount(() => {
+		document.body.classList.add(dark);
+	});
+
+	onDestroy(() => {
+		document.body.classList.remove(dark);
+	});
+</script>
+
+<section class="unlimited-decks">
+	<slot />
+</section>
