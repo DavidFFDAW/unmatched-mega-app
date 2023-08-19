b52b99988167923e32163fdd5f155eb3b5a9dc43
DavidFFDAW <davidferflo2@gmail.com>
2023-08-20 01:31:41 +0200

    feat: unlimited decks card slider

--- src/routes/unlimited-decks/[id]/+page.svelte
+++ src/routes/unlimited-decks/[id]/+page.svelte
@@ -33,9 +33,11 @@
 	{#if $deck.deckData}<h1 class="fff league">Mazo {$deck.deckData.name}</h1>{/if}
 
 	<div class="flex center acenter mega-container">
-		<div class="cards-container">
+		<div class="cards-container slider">
 			{#each $deck.hand as item}
-				<UnmatchedRealCard card={item} on:cardclick={() => selectCard(item)} />
+				<div class="slide">
+					<UnmatchedRealCard card={item} on:cardclick={() => selectCard(item)} />
+				</div>
 			{/each}
 		</div>
 	</div>
@@ -53,8 +55,27 @@
 	}
 
 	@media only screen and (max-width: 768px) {
-		.cards-container {
-			grid-template-columns: repeat(1, 0fr);
+		.cards-container.slider {
+			display: flex;
+			grid-template-columns: unset;
+			width: 100%;
+			display: flex;
+			overflow-x: auto;
+			overflow-y: hidden;
+			min-height: 350px;
+			margin: 0 72px;
+			scroll-snap-type: x mandatory;
+			scroll-behavior: smooth;
+		}
+
+		.cards-container.slider .slide {
+			scroll-snap-align: start;
+			width: 100%;
+			object-fit: cover;
+		}
+
+		.slider::-webkit-scrollbar {
+			display: none;
 		}
 	}
 </style>
