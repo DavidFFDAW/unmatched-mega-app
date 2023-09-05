e0d230b5865ef3a27b690e3fa1b3ea6eaf34d943
DavidFFDAW <davidferflo2@gmail.com>
2023-09-05 09:57:33 +0200

    cl: svelte.config alias configuration

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,16 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			'@src': 'src',
+			'@models': 'src/models',
+			'@components': 'src/components',
+			'@layouts': 'src/layouts',
+			'@pages': 'src/pages',
+			'@shared': 'src/shared',
+			'@services': 'src/services',
+		}
 	}
 };
 
