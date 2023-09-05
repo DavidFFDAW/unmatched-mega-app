ba8271dbddf7165745740003d87c216d36e49301
DavidFFDAW <davidferflo2@gmail.com>
2023-09-05 09:59:17 +0200

    feat: loading spinner and suspense components

---
+++ src/components/loading/global-loading.svelte
@@ -0,0 +1,29 @@
+<section class="global-loading">
+    <div class="spinner"/>
+</section>
+
+<style>
+    .global-loading {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100;
+        background: rgba(0,0,0,.8);
+        z-index: 100;
+    }
+
+    .global-loading .spinner {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 100px;
+        height: 100px;
+        margin: -50px 0 0 -50px;
+        border-radius: 50%;
+        border: 3px solid transparent;
+        border-top-color: #fff;
+        animation: spin 1s linear infinite;
+    }
+
+</style>
---
+++ src/components/suspense/suspense.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import GlobalLoading from '@components/loading/global-loading.svelte';
+    export let component = undefined;
+</script>
+
+{#if CardBackPage}
+	<svelte:component this={CardBackPage} />
+{:else}
+	<GlobalLoading/>
+{/if}
