7c7f39c0a725ce1e6cf1e5faa898362d182b865a
DavidFFDAW <davidferflo2@gmail.com>
2023-09-05 11:12:13 +0200

    reactor: models

--- src/models/api.model.ts
+++ src/models/api.model.ts
@@ -1,8 +1,3 @@
-export type CardPrint = {
-	name: string;
-	url: string;
-};
-
 export type ApiResponse = {
 	status: number;
 	ok: boolean;
---
+++ src/models/card.model.ts
@@ -0,0 +1,12 @@
+export interface CardPrint {
+    name: string;
+    url: string;
+};
+
+export interface CardBack {
+    deck: string;
+    border: string;
+    image: string;
+    letters: string;
+    background: string;
+}
