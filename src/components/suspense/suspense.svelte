b863e2ac98167b272a7259283b14c5c1d3ca4211
DavidFFDAW <davidferflo2@gmail.com>
2023-09-07 13:42:09 +0200

    feat: transition Suspense component

--- src/components/suspense/suspense.svelte
+++ src/components/suspense/suspense.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+	import PageContainer from '@components/container/page-container.svelte';
 	import GlobalLoading from '@components/loading/global-loading.svelte';
 	export let component: any = undefined;
 </script>
 
 {#if component}
-	<svelte:component this={component} />
+	<PageContainer>
+		<svelte:component this={component} />
+	</PageContainer>
 {:else}
 	<GlobalLoading />
 {/if}
