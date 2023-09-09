b7627352b2cb4e727f9be2dc1fbd6491e5de4259
DavidFFDAW <davidferflo2@gmail.com>
2023-09-09 17:05:57 +0200

    feat: character card creator panel

--- src/pages/characters/character.svelte
+++ src/pages/characters/character.svelte
@@ -3,31 +3,52 @@
 	import ButtonFill from '@components/buttons/button-fill.svelte';
 	import Input from '@components/forms/input.svelte';
 	import Switch from '@components/forms/switch.svelte';
+	import HeroCard from '@components/hero-card/hero-card.svelte';
+	import type { HeroCardInterface } from '@components/hero-card/hero.model';
+	import { downloadPngFromElement } from '@services/dom.screenshot.service';
+
+	let datas: HeroCardInterface = {
+		hasSidekick: false,
+		hero: {
+			name: 'Elektra',
+			attack: 'melee',
+			ability:
+				'Cuando Elektra sea derrotada quita a Elektra y a todos los miembros de la mano del tablero. No ha sido derrotada. Al inicio de tu próximo turno, resucítala. Ignora los efectos de RESUCITADA.',
+			hp: 7,
+			move: 2,
+			quote: 'Me pica el culo'
+		},
+		sidekick: {
+			quantity: 1,
+			name: 'Daredevil',
+			attack: 'melee',
+			hp: 5
+		}
+	};
+
+	const downloadCard = (ev: Event) => {
+		ev.preventDefault();
+		return downloadPngFromElement(
+			document.getElementById('unmatchedHeroCard'),
+			`unmatched-hero-card-${datas.hero.name}`
+		);
+	};
 </script>
 
 <div class="flex center acenter gap flex-responsive-reverse">
 	<div class="w1 box p">
 		<h3 class="title">Datos</h3>
 		<form class="flex center astart column gap-smaller">
-			<Input label="Nombre del mazo" name="deck_name" value={''} />
-			<Input label="Título de carta" name="card_title" value="" />
-			<Input label="Personaje" name="character" value="" />
-			<Input type="number" label="Cantidad" name="qty" value="" />
-
-			<div class="w1 form-item">
-				<span class="label">Imagen</span>
-				<div style="margin-top: 5px;">
-					<!-- <ButtonFile bind:image={$cardData.image} /> -->
-				</div>
-			</div>
+			<Input label="Nombre del Héroe" name="hero_name" bind:value={datas.hero.name} />
+			<Input label="Vida del héroe" name="hero_hp" bind:value={datas.hero.hp} />
 		</form>
 	</div>
-	<div id="unmatched-translate-card">
-		<div id="unmatchedTranslateCard">
-			<!-- <TranslateCard bind:cardData={$cardData} /> -->
+	<div id="unmatched-hero-card">
+		<div id="unmatchedHeroCard">
+			<HeroCard {datas} />
 		</div>
 		<div class="flex center acenter">
-			<!-- <ButtonFill label="Descargar carta" click={functions.downloadCard} /> -->
+			<ButtonFill label="Descargar carta" click={downloadCard} />
 		</div>
 	</div>
 </div>
