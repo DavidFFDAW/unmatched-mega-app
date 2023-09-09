025977bfc4f43fcd7a0d644d3bb70e2c64c21092
DavidFFDAW <davidferflo2@gmail.com>
2023-09-09 14:36:14 +0200

    wip: hero card

--- src/pages/unlimited/unlimited.constants.ts
+++ src/pages/unlimited/unlimited.constants.ts
@@ -1,12 +1,12 @@
-import DeckFooter from "./game/components/footers/deck-footer.svelte";
-import DiscardFooter from "./game/components/footers/discard-footer.svelte";
-import HandFooter from "./game/components/footers/hand-footer.svelte";
+import DeckFooter from './game/components/footers/deck-footer.svelte';
+import DiscardFooter from './game/components/footers/discard-footer.svelte';
+import HandFooter from './game/components/footers/hand-footer.svelte';
 
 export const PAGES: any = {
 	hand: 'hand',
 	discard: 'discard',
 	deck: 'deck',
-	info: 'deckinfo'
+	info: 'info'
 };
 
 export const TABS: any = {
@@ -20,5 +20,5 @@ export const FOOTER_TABS = {
 	[PAGES.hand]: HandFooter,
 	[PAGES.discard]: DiscardFooter,
 	[PAGES.deck]: DeckFooter,
-	'default': HandFooter,
-}
+	default: HandFooter
+};
