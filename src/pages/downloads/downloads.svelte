6ad5cc77ec359d48fbfcc7fa65fd0fbf85a11907
DavidFFDAW <davidferflo2@gmail.com>
2024-01-11 00:39:47 +0100

    feat: downloads page with files uploaded

--- src/pages/downloads/downloads.svelte
+++ src/pages/downloads/downloads.svelte
@@ -1,3 +1,31 @@
 <div class="landing-page">
-    <h1>Aquí podrás acceder a todos aquellos archivos que pueden ser útiles y se podrán descargar</h1>
+	<h2>Archivos disponibles para descargar:</h2>
+
+	<div class="down w1 flex column">
+		<ul class="w1 flex column center astart gap-small">
+			<li>
+				<a href="/downloads/Arthur.odt" download>Plantilla de mazo para impresión</a>
+			</li>
+			<li>
+				<a href="/downloads/card-back-template.psd" download
+					>PSD template de parte de atras de la carta</a
+				>
+			</li>
+			<li>
+				<a href="/downloads/card-art-template.psd" download> Ajuste de imagen de cartas preview</a>
+			</li>
+			<li>
+				<a href="/downloads/dial-template.psd" download>Plantilla de dial</a>
+			</li>
+			<li>
+				<a href="/downloads/cuphead-dial.xcf" download>XCF Dial de Cuphead</a>
+			</li>
+			<li>
+				<a href="/downloads/among-us-dial.xcf" download>XCF Dial de Among Us</a>
+			</li>
+			<li>
+				<a href="/downloads/hollow-knight-dial.xcf" download>XCF Dial de Hollow Knight</a>
+			</li>
+		</ul>
+	</div>
 </div>
