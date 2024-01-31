3514bea362d0966770c27310df593cd5fbc59f5e
DavidFFDAW <davidferflo2@gmail.com>
2024-01-31 14:45:29 +0100

    feat: toggle box

---
+++ src/components/boxed/toggle-box.svelte
@@ -0,0 +1,11 @@
+<script>
+    let showBox = false;
+</script>
+
+<div class="w1 box p relative {showBox ? 'show-box' : 'unshow-box'}">
+    <button class="absolute btn-absolute-show-box-toggle">
+        {showBox ? 'Mostrar' : 'Ocultar'}
+    </button>
+    
+    <slot/>
+</div>
