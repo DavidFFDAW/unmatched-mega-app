b4a317bc9a542df1cbc4657ac0a0a41045743964
DavidFFDAW <davidferflo2@gmail.com>
2024-02-07 10:39:36 +0100

    feat: print-info-block component and updated datas

---
+++ src/pages/tuckbox/components/print-info-block.svelte
@@ -0,0 +1,35 @@
+<script lang="ts"></script>
+
+<div class="w1 box p">
+    <h3 class="title" id="printing-datas">Datos importantes antes de imprimir</h3>
+    <ul class="flex column gap-5">
+        <li>Las cartulinas de <span class="copicentro">copicentro</span> no son viables puesto que se agrietan muy fácilmente.</li>
+        <li>Las pruebas que se han hecho han sido en un <a href="https://www.copyrap.com/franquiciados/" class="copyrap" target="_blank">copyrap</a>, concretamente el de <a href="https://www.copyrap.com/franquiciados/nueva-malaga/" target="_blank" class="copyrap">Nueva Málaga</a>.</li>
+        <li>Se tiene en cuenta que las cartas llevan <strong>funda</strong> y por eso la caja es bastante más grande (69mm) que el tamaño de una carta normal del juego (63mm).</li>
+        <li>La impresión se realiza sobre una cartulina de entre <strong>200g</strong> y <strong>250g</strong> con brillo <span class="carefull">(se trata de que sea consistente pero que tampoco sea muy grueso para que no quite espacio)</span>.</li>
+        <li>El tamaño de la caja es de <strong>69x92mm</strong> con una altura variable según el número de cartas que indiquemos.</li>
+        <li>La proporción para el espacio hacia arriba se basa en que para 31 cartas hay 24mm de altura (suficiente para las cartas). Se calcula mediante una regla de tres.</li>
+        <li>El PDF generado tiene el tamaño ya calculado, <strong>NO</strong> es necesario ajustar nada más</li>
+        <li>Es bastante posible que las imágenes pierdan <strong>calidad</strong> al convertirse en <strong>PDF</strong> pero es inevitable por la forma en la que trabaja internamente la web.</li>
+        <li>Una vez impresa la caja cabe en las <strong>posiciones</strong> de las cartas dentro de las cajas de cada set de unmatched. Probado en: 
+            <ul class="flex column gap-5" style="margin-top: 5px;">
+                <li>UNmatched: Caperucita Roja vs. Beowulf.</li>
+                <li>UNmatched: Battle of Legends, Volume Two.</li>
+            </ul>
+        </li>
+    </ul>
+</div>
+
+<style>
+	.copyrap {
+		color: #237f3c;
+	}
+
+	.copicentro {
+		color: #3193e4;
+	}
+
+	.carefull {
+		color: #b48631;
+	}
+</style>
